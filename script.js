9f3cdc33ed78db1ad82daa0df6874f5d343ca8a7
Vincent Yono <vincentyono@protonmail.com>
2024-02-07 18:28:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const adviceId = document.getElementById("advice-id");
+const adviceContent = document.getElementById("advice-content");
+const adviceButton = document.getElementById("button");
+const spinner = document.getElementById("spinner");
+
+const getAdvice = async () => {
+  adviceContent.innerHTML = `<div id="spinner" class="lds-ring">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>`;
+  spinner.style = "display: block;";
+
+  const response = await fetch("https:/api.adviceslip.com/advice", {
+    method: "GET",
+  });
+
+  const advice = await response.json();
+
+  spinner.style = "display: none;";
+
+  const id = advice["slip"]["id"];
+  const content = advice["slip"]["advice"];
+
+  adviceId.innerText = `Advice #${id}`;
+  adviceContent.innerHTML = `"${content}"`;
+
+  return advice;
+};
+
+getAdvice();
+
+adviceButton.addEventListener("click", getAdvice);
